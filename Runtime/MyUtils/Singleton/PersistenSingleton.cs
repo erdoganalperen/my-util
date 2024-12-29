3e1e94fd748126d80887d83978babac412e91ea1
alperenerdogan <alperenerdogan0505@gmail.com>
2024-12-29 20:21:38 +0300

    singleton

---
+++ Runtime/MyUtils/Singleton/PersistenSingleton.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+
+namespace vexus_utility.Runtime.MyUtils.Singleton
+{
+    // singleton from git-amend
+    public class PersistentSingleton<T> : MonoBehaviour where T : Component
+    {
+        private static T _instance;
+
+        public static bool HasInstance => _instance != null;
+        public static T TryGetInstance() => HasInstance ? _instance : null;
+
+        public static T Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = FindAnyObjectByType<T>();
+                    if (_instance == null)
+                    {
+                        var go = new GameObject(typeof(T).Name + " Auto-Generated");
+                        _instance = go.AddComponent<T>();
+                    }
+                }
+
+                return _instance;
+            }
+        }
+
+        /// <summary>
+        /// Make sure to call base.Awake() in override if you need awake.
+        /// </summary>
+        protected virtual void Awake()
+        {
+            InitializeSingleton();
+        }
+
+        protected virtual void InitializeSingleton()
+        {
+            if (!Application.isPlaying) return;
+            
+            if (_instance == null)
+            {
+                _instance = this as T;
+                DontDestroyOnLoad(gameObject);
+            }
+            else
+            {
+                if (_instance != this)
+                {
+                    Destroy(gameObject);
+                }
+            }
+        }
+    }
+}
